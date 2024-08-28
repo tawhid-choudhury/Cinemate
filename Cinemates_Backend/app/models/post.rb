fd9ccb7e79cae21911d84895b06c60ac06ed9d92
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-08-28 20:50:17 +0600

    created association with movies, increment/decrement of post count

--- Cinemates_Backend/app/models/post.rb
+++ Cinemates_Backend/app/models/post.rb
@@ -1,4 +1,20 @@
 class Post < ApplicationRecord
 
   belongs_to :user
+  belongs_to :movie
+
+  after_create :increment_movie_post_count
+  after_destroy :decrement_movie_post_count
+
+  private
+
+  def increment_movie_post_count
+    movie_post_count = MoviePostCount.find_or_create_by(movie: movie)
+    movie_post_count.increment!(:post_count)
+  end
+
+  def decrement_movie_post_count
+    movie_post_count = MoviePostCount.find_by(movie: movie)
+    movie_post_count.decrement!(:post_count) if movie_post_count
+  end
 end
