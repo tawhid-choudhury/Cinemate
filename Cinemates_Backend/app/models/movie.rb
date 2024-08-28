d9314426816fd172e597dbdb6ebdf459b4c6fee9
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-08-28 20:49:04 +0600

    added association with post model

--- Cinemates_Backend/app/models/movie.rb
+++ Cinemates_Backend/app/models/movie.rb
@@ -1,10 +1,11 @@
 class Movie < ApplicationRecord
 
+  has_many :watchlists, dependent: :destroy
+  has_many :cinemates_ratings, dependent: :destroy
+  has_many :posts, dependent: :destroy
+
   def age
     return 0 unless released
     ((Time.zone.now - released.to_time) / 1.year.seconds).floor
   end
-
-  has_many :watchlists, dependent: :destroy
-  has_many :cinemates_ratings, dependent: :destroy
 end
